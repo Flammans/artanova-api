20e6aa2e2285de06111726fd9d337f37318f5a40
Viktor Khasenko <developer@viktorkhasenko.com>
2024-09-11 00:46:47 +0100

    Add full-text search capability to artwork queries

--- routes/artworks/index.get.ts
+++ routes/artworks/index.get.ts
@@ -2,20 +2,30 @@ import {defineEventHandler, getValidatedQuery} from 'h3';
 import {z} from 'zod';
 
 export default defineEventHandler(async (event) => {
-  const query = await getValidatedQuery(event, z.object({
+  const {limit, cursor, query} = await getValidatedQuery(event, z.object({
     limit: z.coerce.number().int().positive().max(1_000).default(10),
     cursor: z.number().optional(),
+    query: z.string().optional(),
   }).parse);
 
   const prisma = usePrisma();
 
   const artworks = await prisma.artwork.findMany({
-    take: query.limit,
-    cursor: query.cursor ? {
-      id: query.cursor,
+    take: limit,
+    cursor: cursor ? {
+      id: cursor,
     } : undefined,
     orderBy: {
       updatedAt: 'desc',
+    },
+    where: {
+      ...(query ? {
+        OR: ['title', 'medium', 'creditLine', 'origin'].map((field) => ({
+          [field]: {
+            search: query,
+          },
+        })),
+      } : {}),
     },
   });
 
