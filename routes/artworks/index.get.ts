dd97063d82eb729cdbf9deabef0af6c4d2befa94
Viktor Khasenko <developer@viktorkhasenko.com>
2024-09-11 02:10:06 +0100

    Refactor sorting logic to separate nullable and non-nullable fields

--- routes/artworks/index.get.ts
+++ routes/artworks/index.get.ts
@@ -9,18 +9,24 @@ const searchFields: Array<keyof Artwork> = [
   'origin',
 ];
 
-const sortFields: Array<keyof Artwork> = [
-  'updatedAt',
-  'yearFrom',
-  'yearTo',
-];
+const sortFields: {
+  [key in 'notNullable' | 'nullable']: Array<keyof Artwork>
+} = {
+  notNullable: [
+    'updatedAt',
+  ],
+  nullable: [
+    'yearFrom',
+    'yearTo',
+  ],
+};
 
 export default defineEventHandler(async (event) => {
   const {limit, cursor, query, sort, order} = await getValidatedQuery(event, z.object({
     limit: z.coerce.number().int().positive().max(1_000).default(10),
     cursor: z.number().optional(),
     query: z.string().optional(),
-    sort: z.enum(sortFields as any).default('updatedAt'),
+    sort: z.enum([...sortFields.nullable, ...sortFields.notNullable] as any).default('updatedAt'),
     order: z.enum(['asc', 'desc']).default('desc'),
   }).parse);
 
@@ -35,9 +41,11 @@ export default defineEventHandler(async (event) => {
       [sort]: order,
     },
     where: {
-      [sort]: {
-        not: null,
-      },
+      ...(sortFields.nullable.includes(sort) ? {
+        NOT: {
+          [sort]: null,
+        },
+      } : {}),
       ...(query ? {
         OR: searchFields.map((field) => ({
           [field]: {
